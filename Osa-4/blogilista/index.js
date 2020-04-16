96894c3162e7314ce407750e4597951a96b166d1
sppirtti <samppa-1997@hotmail.com>
2020-04-16 18:07:33 +0300

    4.1 toimii lisäys ja näkyy localissa

---
+++ Osa-4/blogilista/index.js
@@ -0,0 +1,52 @@
+const http = require('http')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+app.use(cors())
+app.use(express.json())
+
+
+const blogSchema = mongoose.Schema({
+    title: String,
+    author: String,
+    url: String,
+    likes: Number
+})
+
+console.log("luotu sceme")
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = 'mongodb+srv://sppirtti:Blapsin_321@cluster0-pkxno.mongodb.net/blogilista-app?retryWrites=true&w=majority'
+
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/blogs', (request, response) => {
+    Blog
+        .find({})
+        .then(blogs => {
+           response.json(blogs.map(blog => blog.toJSON())) 
+        })
+            
+})
+
+app.post('/api/blogs', (request, response) => {
+    const blog = new Blog(request.body)
+
+    blog
+        .save()
+        .then(result => {
+            response.status(201).json(result)
+        })
+})
+
+const PORT = 3003
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
