7d322f227f3e452c6bc6f3de0eb83837c7c909b3
sppirtti <samppa-1997@hotmail.com>
2020-04-23 17:57:03 +0300

    front ja back sopii yhteen

---
+++ Osa-5/blogilista/bloglist-backend/models/blog.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+mongoose.set('useFindAndModify',false)
+mongoose.set('useCreateIndex', true)
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref:'User'
+  }
+})
+
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+
+    if (returnedObject.likes === undefined) {
+      returnedObject.likes = 0
+    }
+  }
+})
+
+module.exports = mongoose.model('Blog', blogSchema)
---
+++ Osa-5/blogilista/bloglist-backend/models/user.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+
+mongoose.set('useFindAndModify', false)
+mongoose.set('useCreateIndex', true)
+
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        unique: true,
+        minlength: 3,
+        required: true
+    },
+    passwordHash: {
+        type: String,
+        required: true
+    },
+    name: String,
+    blogs: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Blog'
+        }
+    ]
+})
+
+userSchema.set('toJSON', {
+
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+        delete returnedObject.passwordHash
+    }
+
+})
+userSchema.plugin(uniqueValidator)
+
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
