f64387b0d4ca2c5237d81fc4ecfe80172d037d80
sppirtti <samppa-1997@hotmail.com>
2020-05-14 13:22:30 +0300

    force add, osa-5 frontend pudonnut gitin piiristä, korjausyritys

---
+++ Osa-2/puhelinluettelo/build/precache-manifest.64bc394e0e2b6a54c65267610de3ead0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ffab1f5250a9aad32d10556684de137",
+    "url": "/index.html"
+  },
+  {
+    "revision": "533afc858571650bdf1e",
+    "url": "/static/css/main.cbbb9da4.chunk.css"
+  },
+  {
+    "revision": "08d3aad3ecae6194104c",
+    "url": "/static/js/2.f518c3b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f518c3b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "533afc858571650bdf1e",
+    "url": "/static/js/main.2416b1a1.chunk.js"
+  },
+  {
+    "revision": "c940798f285500efd361",
+    "url": "/static/js/runtime-main.64749484.js"
+  }
+]);
